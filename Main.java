1463797968d5beb2e4eb49be73b31c90d7f74554
Marcin Lewicki <lemarcin@gmail.com>
2021-12-29 10:22:17 -0500

    Work on project. Stage 2/5: Sold!

--- Main.java
+++ Main.java
@@ -1,33 +1,22 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        char[][] seatingPlan = enterSeats();
-        print(seatingPlan);
-    }
-
-    public static char[][] enterSeats() {
-        final int rows = 7;
-        final int seats = 8;
-        char[][] seatingPlan = new char[rows][seats];
-        for (int i = 0; i < seatingPlan.length; i++) {
-            for (int j = 0; j < seatingPlan[i].length; j++) {
-                seatingPlan[i][j] = 'S';
-            }
-        }
-        return seatingPlan;
-    }
-
-    public static void print(char seatingPlan[][]) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 0; i < seatingPlan.length; i++) {
-            System.out.print(i + 1 + " ");
-            for (int j = 0; j < seatingPlan[i].length; j++) {
-                System.out.print(seatingPlan[i][j] + " ");
-            }
-            System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        System.out.print("> ");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        System.out.print("> ");
+        int seats = scanner.nextInt();
+        System.out.println("Total income:");
+        if (rows * seats <= 60) {
+            System.out.println("$" + rows * seats * 10);
+        } else {
+            System.out.println("$" + (seats * (rows / 2 * 10 + (rows - rows / 2) * 8)));
         }
     }
 }
